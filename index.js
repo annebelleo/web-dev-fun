6a345ae7a215c7877d2fe0be1c049447a17cc2a4
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2018-06-28 18:12:02 +0000

    Added DOM

---
+++ index.js
@@ -0,0 +1,12 @@
+function surprise() {
+    document.getElementById("surpriseText").innerHTML = "Ta-da!"
+}
+
+function alertFunction() {
+    alert("This is an alert. How cool!")
+}
+
+function response() {
+    var response = prompt("Type your response here:");
+    document.getElementById("responseText").innerHTML = response;
+}
