1f9948ebb9afd1b6ae20bc3e77f3b2b79c702364
Lourens Touwen <touwenlourens@gmail.com>
2018-10-04 09:45:31 +0200

    impressive coding added

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ function response() {
     var response = prompt("Type your response here:");
     document.getElementById("responseText").innerHTML = response;
     document.getElementById("hiddenText").style.visibility = "visible";
+    console.log('sup')
 }
